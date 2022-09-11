5462bda986a89a5aefced767f27e9fa3c8fd23c0
Leonard-N <mmolotsi27@gmail.com>
2022-09-11 08:28:16 -0700

    P+or-N

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 /**
  * main: will assign a random number to the variable n.
+ * positive anything is better than negative nothing.
  *
  * Description: print whether the nmuber is positive, negative or zero.
  * Return: 0.
@@ -11,22 +12,21 @@
 
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf('%i is positive\n', n);
-	}
-	else if (n ==0)
-	{
-		printf('%i is zero\n', n);
-	}
-	else if (n < 0)
-	{
-		printf('%i is negative\n', n);
-	}
-	return (0);
-
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf('%i is positive\n', n);
+}
+else if (n ==0)
+{
+printf('%i is zero\n', n);
+}
+else
+{
+printf('%i is negative\n', n);
+}
+return (0);
 }

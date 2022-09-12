4ef6ee69a3dad69146ab30028428688b1a5e7c37
Leonard-N <mmolotsi27@gmail.com>
2022-09-12 12:36:53 -0700

    insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,20 +3,20 @@
 /**
  * main - int main(void)
  *
- *
  * Description: print alphabet lowecase
  * Return: 0
  */
 
-int main (void)
+int main(void)
 {
 	char c;
 
 	for (c = "a"; c <= "z"; c++)
+	{
 		putchar(c);
+	}
 
 	putchar("\n");
 
 	return (0);
-
 }

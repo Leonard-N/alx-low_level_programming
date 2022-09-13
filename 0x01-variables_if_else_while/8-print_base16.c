a49d850c690adbf543b221b2bb33240cfe78b10a
Leonard-N <mmolotsi27@gmail.com>
2022-09-12 17:17:44 -0700

    task-8_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 /**
+ * main - Prints all the numbers of base 16 in lowercase.
  *
- ** main - Prints all the numbers of base 16 in lowercase.
- *
- **
- *
- ** Return: Always 0.
- *
- **/
+ * Description: 
+ * Return: 0
+ */
 
 int main(void)
 

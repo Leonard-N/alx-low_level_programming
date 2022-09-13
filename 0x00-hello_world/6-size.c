8efd7ce7685f4731b01b80e82ee6c3a13a5e6895
Leonard-N <mmolotsi27@gmail.com>
2022-09-12 23:13:31 -0700

    plz work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - int main(void)
- * Description:nProgramming is like building a multilingual puzzle.
- *
- * Return: 0
- */
+* main - int main(void)
+*
+* Description:nProgramming is like building a multilingual puzzle.
+* Everything takes time to learn
+* Return: 0
+*/
 int main(void)
 {
 	char x;
 	int z;
-	long int y;
-	long long int w;
+	long y;
+	long long w;
 	float q;
 
-	printf('Size of a char: %lu byte(s)\n', sizeof(x));
-	printf('Size of an int: %lu byte(s)\n', sizeof(z));
-	printf('Size of a long int: %lu byte(s)\n', sizeof(y));
-	printf('Size of a long long int: %lu byte(s)\n', sizeof(w));
-	printf('Size of a float: %lu bytes(s)\n', sizeof(q));
+	printf('Size of a char: %ld byte(s)\n', sizeof(x));
+	printf('Size of an int: %ld byte(s)\n', sizeof(z));
+	printf('Size of a long int: %ld byte(s)\n', sizeof(y));
+	printf('Size of a long long int: %ld byte(s)\n', sizeof(w));
+	printf('Size of a float: %ld bytes(s)\n', sizeof(q));
 	return (0);
-
 }

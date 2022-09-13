89e5b1d02a427df4435a460812166e6e1c827ca2
Leonard-N <mmolotsi27@gmail.com>
2022-09-12 22:19:52 -0700

    hello_task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,8 @@
 
 /**
  * main - int main(void)
- *Description: the output should be Programming is like building a multilingual puzzle.
-
+ * Description: the output should be Programming is like building a multilingual puzzle.
+ *
  * Return: 0
  */
 int main(void)
@@ -14,11 +14,11 @@ int main(void)
 	long long int w;
 	float q;
 
-	printf('Size of a char: %ld byte(s)\n', sizeof(x));
-	printf('Size of an int: %ld byte(s)\n', sizeof(z));
-	printf('Size of a long int: %ld byte(s)\n', sizeof(y));
-	printf('Size of a long long int: %ld byte(s)\n', sizeof(w));
-	printf('Size of a float: %ld bytes(s)\n', sizeof(q));
+	printf('Size of a char: %lu byte(s)\n', (unsigned)sizeof(x));
+	printf('Size of an int: %lu byte(s)\n', (unsigned)sizeof(z));
+	printf('Size of a long int: %lu byte(s)\n', (unsigned)sizeof(y));
+	printf('Size of a long long int: %lu byte(s)\n', (unsigned)sizeof(w));
+	printf('Size of a float: %lu bytes(s)\n', (unsigned)sizeof(q));
 	return (0);
 
 }

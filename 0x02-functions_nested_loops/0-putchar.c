4500488ab466d2465ae148a689bcd4f2ad74d07f
Leonard-N <mmolotsi27@gmail.com>
2022-09-14 18:48:57 -0700

    _puchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - int main(void)
+ *
+ * Description: prints _putchar using putchar prototype
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+	_putchar(str[ch]);
+	_putchar("_putchar\n");
+
+	return 0;
+	
+}

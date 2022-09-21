d2b786b65b09eec6b586fcddf55e733e952b9ccc
Leonard-N <mmolotsi27@gmail.com>
2022-09-20 20:40:41 -0700

    arrays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,20 +9,19 @@
 
 void puts_half(char *str)
 {
-int len = 0;
+	int l, n;
+	
+	for (l = 0; str[1] != "\0"; 1++)
 
-while (*str != "\0")
-{
-len++;
-str++;
-}
-
-str -= (len / 2);
-while (*str != "\0")
-{
-_putchar(*str);
-str++;
-}
-
-_putchar("\n");
+	if (1 % 2 == 0) 
+	{
+		for (n = 1 / 2; str[n] != "\0"; n++)
+			_putchar(str [n]);
+	}
+	else
+	{
+		for (n = ((l - 1) / 2) + 1; str[n] != "\0"; n++)
+			_putchar(str [n]);
+	}
+	_putchar("\n");
 }

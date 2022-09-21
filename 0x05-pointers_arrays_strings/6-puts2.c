341532af080d510f71ca7bb241a2772c320cebe6
Leonard-N <mmolotsi27@gmail.com>
2022-09-21 13:59:36 -0700

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <string.h>
 
 /**
-* puts2 -  prints characters of a string 
+* puts2 - prints characters of a string 
 * @str: the output will ba a sting
 *
 * Return: void
@@ -9,15 +10,10 @@
 
 void puts2(char *str)
 {
-	int i;
+	int len, i
 
-	while (str[i] != '\0')
-	{
-	if (i % 2 == 0)
-
-	_putchar(str[i]);
-	i++;
-	}
-
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }

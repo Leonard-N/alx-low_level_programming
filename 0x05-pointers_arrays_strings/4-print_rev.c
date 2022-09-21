36fd0216fd3c5810ac2af3b04dbb84669cd07ad8
Leonard-N <mmolotsi27@gmail.com>
2022-09-20 19:01:06 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,52 +3,26 @@
 /**
 * print_rev -  prints a string in reverse
 * @s: the output will be string
+*
 * Return: void
 */
 
 void print_rev(char *s)
-
 {
-
 	int i;
 
-
-
 	while (s[i] != '\0')
-
 	{
-
-		i++;
-
+	i++;
 	}
-
-
 
 	i--;
 
-
-
 	while (s[i] != '\0')
-
 	{
-
 	_putchar(s[i--]);
-
 	}
 
 	_putchar('\n');
 
 }
-
-Footer
-
-© 2022 GitHub, Inc.
-
-Footer navigation
-
-Terms
-
-Privacy
-
-Security
-

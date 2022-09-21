ceca5def2ca836ff6c4af8a612774f0fef51f3b1
Leonard-N <mmolotsi27@gmail.com>
2022-09-20 19:55:39 -0700

    loops

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,8 @@ void print_rev(char *s)
 {
 	int z;
 
-	for(z = 0; s[z] != '\0'; z++)
-	
-	for(--z; z >= 0; --z)
+	for (z = 0; s[z] != '\0'; z++)
+	for (--z; z >= 0; --z)
 		_putchar(s[z]);
 
 	_putchar('\n');

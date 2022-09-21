1abb73f75eb4b1c3996444c05c3737e7a7eaaf61
Leonard-N <mmolotsi27@gmail.com>
2022-09-20 20:15:06 -0700

    loops

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,28 +1,23 @@
 #include "main.h"
 
 /**
-* * puts2 -  prints a s* @str: the output will ba a sting
-* Return: string to stdout
+* puts2 -  prints characters of a string 
+* @str: the output will ba a sting
+*
+* Return: void
+*/
 
 void puts2(char *str)
-
 {
-
 	int i;
 
-
-
 	while (str[i] != '\0')
-
 	{
+	if (i % 2 == 0)
 
-		if (i % 2 == 0)
-
-			_putchar(str[i]);
-
-		i++;
-
+	_putchar(str[i]);
+	i++;
 	}
 
 	_putchar('\n');
-
+}

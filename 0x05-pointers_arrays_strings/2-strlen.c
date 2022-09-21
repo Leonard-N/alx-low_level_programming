3c79b764a7eb1a6137cef9c1fef27a5f6f4e748b
Leonard-N <mmolotsi27@gmail.com>
2022-09-20 18:46:17 -0700

    pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,7 @@
 /**
  * strlen - returns the length of a string
  * @s: arg points to string
+ * Description: _strlen its fucntion
  *
  * Return: string length
  */
@@ -11,7 +12,7 @@ int _strlen(char *s)
 {
 	int len;
 
-	for(len = 0; *s != '\0'; ++s)
+	for (len = 0; *s != '\0'; ++s)
 	{
 		len++;
 	}

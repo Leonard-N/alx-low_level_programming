4057c6a926dc35b1dc95e97341ff56da170af882
Leonard-N <mmolotsi27@gmail.com>
2022-09-21 14:31:13 -0700

    loops

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
 * puts_half - prints a string
@@ -9,19 +10,14 @@
 
 void puts_half(char *str)
 {
-	int l, n;
-	
-	for (l = 0; str[l] != "\0"; l++)
+	int x, y, i;
 
-	if (l % 2 == 0) 
-	{
-		for (n = l / 2; str[n] != "\0"; n++)
-			_putchar(stri[n]);
-	}
+	x = strlen(str);
+	if (x % 2 ==1)
+		y = x / 2 + 1;
 	else
-	{
-		for (n = ((l - 1) / 2) + 1; str[n] != "\0"; n++)
-			_putchar(str[n]);
-	}
-	_putchar("\n");
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }

ba51d0522547309396c85181c1cdf1968fb65325
Leonard-N <mmolotsi27@gmail.com>
2022-09-22 15:54:20 -0700

    0.strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* strcat - This function appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
+* @dest:
+* @src:
+*
+* Return: A pointer to th destination string @dest.
+*/
+
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}

d93d5b35241fe45b3096f80ee1d36524853a7019
Leonard-N <mmolotsi27@gmail.com>
2022-09-27 05:43:17 -0700

    _mumcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _memcpy - copies memory area.
+* @dest: char pointer
+* @src: char pointer
+* @n: unsigned int
+*
+* Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int index;
+	unsigned char *de_st = dest;
+	const unsigned char *source = src;
+	
+	for (index = 0; index < n; index++)
+	{
+		de_st[index] = source[index];
+	}
+
+	return (dest);
+}

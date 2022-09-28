41619077fa69566f49e15ac45e71504685c19ead
Leonard-N <mmolotsi27@gmail.com>
2022-09-27 17:43:30 -0700

    char_str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _2-strchr.c - Entry point
+ * @s: string pointed to
+ * @c: input
+ *  
+ * Return: s
+ */
+
+char *_strchr(char *s, char c);
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}

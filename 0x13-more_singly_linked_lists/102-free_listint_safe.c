311d92b2fdf06e24a09d1437bf5d72e2d4895ecf
Leonard-N <mmolotsi27@gmail.com>
2022-10-25 15:10:52 -0700

    safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
 /**
-* free_listint_safe - Frees a listint_t list
-* @h: listint_t double pointer
-*
-* Return: size_t
-*/
+ * free_listint_safe - Frees a listint_t list
+ * @h: listint_t double pointer
+ *
+ * Return: size_t
+ */
 
 size_t free_listint_safe(listint_t **h)
 {
 	size_t count = 0;
 	int count2;
+
 	listint_t *head, *new_node;
 
 	if (*h == NULL)

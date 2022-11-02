e8564ab9184adb906ae41b471c873257b836c63a
Leonard-N <mmolotsi27@gmail.com>
2022-11-02 07:15:37 -0700

    file_i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints it.
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
  * main - Copies content of file to another
